0ffc4efe0434d1747cc190b55d778f2d71cfb439
Said-Alisic <said0390@stud.kea.dk>
2019-04-19 14:18:31 +0200

    Flask app#1 without SQL database

---
+++ data.py
@@ -0,0 +1,25 @@
+def articles():
+    articles = [
+        {
+            "id": 1,
+            "title": "Article One",
+            "Body": "Some stuff is written here",
+            "author": "Brady Traversy",
+            "create_date":"04-25-2017"
+        },
+        {
+            "id": 2,
+            "title": "Article Two",
+            "Body": "Some stuff is written here",
+            "author": "Brady Traversy",
+            "create_date": "04-25-2017"
+        },
+        {
+            "id": 3,
+            "title": "Article Three",
+            "Body": "Some stuff is written here",
+            "author": "Brady Traversy",
+            "create_date": "04-25-2017"
+        }
+    ]
+    return articles
